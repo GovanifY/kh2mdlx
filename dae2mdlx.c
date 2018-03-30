092b59161844c866a3308ae26e2cfda06a91b77c
Gauvain Roussel-Tarbouriech <govanify@anarchist.pw>
2018-03-31 00:20:32 +0200

    Primitive mdlx building

--- dae2mdlx.c
+++ dae2mdlx.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
  * Here is an high-level overview of the MDLX file format
@@ -14,7 +15,7 @@
  * | |   |MDL_P_H|   | |
  * | |   | ...   |   | |
  * | |   |-------|   | |
- * | |     ?????     | |
+ * | |     ?????     | | <- unused in KH2, used in KH1
  * | |     MDL_P     | |
  * | | |-----------| | |
  * | | |   BONES   | | |
@@ -110,7 +111,7 @@ struct mdl_header {
     unsigned short bone_cnt;
     unsigned short unk1;
     unsigned int bone_off;
-    unsigned int unk2;
+    unsigned int unk_off;;
     unsigned short mdl_subpart_cnt; 
     unsigned short unk3; 
 };
@@ -169,12 +170,20 @@ int main(int argc, char* argv[]){
 
         struct mdl_header *head=malloc(sizeof(struct mdl_header));
         struct mdl_subpart_header *subh=malloc(sizeof(struct mdl_subpart_header));
-        mdl_header->nmb=3;
-        mdl_header->bone_cnt=1;
-        mdl_header->mdl_subpart_cnt=1;
+        head->nmb=3;
+        head->bone_cnt=1;
+        head->mdl_subpart_cnt=1;
 
+        // write here dummy headers, will need to modify their offsets later
+        int off_head = ftell(mdl);
+        fwrite(head , 1 , sizeof(struct mdl_header) , mdl);
+        int off_subh = ftell(mdl);
+        fwrite(subh , 1 , sizeof(struct mdl_subpart_header) , mdl);
+
+
+        int off_bone = ftell(mdl);
         struct bone_entry *bone=malloc(sizeof(struct bone_entry));
-        bone->idx=1;
+        bone->idx=0;
         bone->parent=-1;
         bone->sca_x=1.0;
         bone->sca_y=1.0;
@@ -185,7 +194,61 @@ int main(int argc, char* argv[]){
         bone->trans_x=1.0;
         bone->trans_y=1.0;
         bone->trans_z=1.0;
-        // todo: create a test DMA struct, mat, update offset 
+        fwrite(bone , 1 , sizeof(struct bone_entry) , mdl);
+
+        FILE * dummy_vif = fopen("fish.kh2v", "rb");
+
+
+        int off_vif = ftell(mdl) - 0x90;
+
+        size_t n, m;
+        unsigned char buff[8192];
+        do {
+            n = fread(buff, 1, sizeof buff, dummy_vif);
+            if (n) m = fwrite(buff, 1, n, mdl);
+            else   m = 0;
+        } while ((n > 0) && (n == m));
+        
+
+        int off_dma = ftell(mdl);
+        // 910
+        char end_dma[] = {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x00, 0x00, 0x00, 0x00};
+        fwrite(end_dma , 1 , sizeof(end_dma) , mdl);
+        unsigned short qwc_len=128;
+        unsigned short qwc_mat_len=4;
+        unsigned short res_unk = 0x3000;
+        unsigned int vif_off=0x0;
+        fwrite(&qwc_len , 1 , sizeof(qwc_len) , mdl);
+        fwrite(&res_unk , 1 , sizeof(res_unk) , mdl);
+        fwrite(&off_vif , 1 , sizeof(off_vif) , mdl);
+		for (int i=0; i<8;i++){fwrite(empty , 1 , sizeof(empty) , mdl);}
+        fwrite(&qwc_mat_len , 1 , sizeof(qwc_len) , mdl);
+        fwrite(&res_unk , 1 , sizeof(res_unk) , mdl);
+		for (int i=0; i<4;i++){fwrite(empty , 1 , sizeof(empty) , mdl);}
+        char stcycl[] = {0x01, 0x01, 0x00, 0x01}; // stcycl 1,1
+        fwrite(stcycl , 1 , sizeof(stcycl) , mdl);
+        // dma writes after vif, in that case 307
+        char qwc_vif_len[] = {246};
+        char unpack[] = {0x80, 0x04, 0x6c}; // unpack V4_32
+        unsigned short unpack_end=0x6c04;
+        fwrite(qwc_vif_len , 1 , sizeof(qwc_vif_len) , mdl);
+        fwrite(unpack , 1 , sizeof(unpack) , mdl);
+        fwrite(end_dma , 1 , sizeof(end_dma) , mdl);
+
+        
+
+        int off_mat = ftell(mdl);
+        unsigned int mat_dummy[] = { 0x01, 0x0, 0x0, 0x0 };
+        fwrite(mat_dummy , 1 , sizeof(mat_dummy) , mdl);
+        
+        // fixing header offsets
+        fseek(mdl, off_head, SEEK_SET);
+        head->bone_off=off_bone + 0x90;
+        fwrite(head , 1 , sizeof(struct mdl_header) , mdl);
+        fseek(mdl, off_subh, SEEK_SET);
+        subh->DMA_off=off_dma + 0x90;
+        subh->mat_off=off_mat + 0x90;
+        fwrite(subh , 1 , sizeof(struct mdl_subpart_header) , mdl);
 
 		fclose(mdl);
 }
